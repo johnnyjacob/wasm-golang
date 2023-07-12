b4ae5fadf32a0051f91395d63466ca6c210361c9
Johnny Jacob <johnnyjacob@gmail.com>
2023-07-12 07:03:28 +0530

    Basic bridge between js and wasm.

--- validate.go
+++ validate.go
@@ -4,34 +4,54 @@ import (
 	"context"
 	"fmt"
 
+	"syscall/js"
+
 	"github.com/open-policy-agent/opa/ast"
 	"github.com/open-policy-agent/opa/rego"
 )
 
-func main() {
+func jsRegoValidateWrapper() js.Func {  
+        jsRegoValidateFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+                if len(args) != 1 {
+                        return "Invalid no of arguments passed"
+                }
+                inputPolicy := args[0].String()
+                fmt.Printf("input %s\n", inputPolicy)
 
-	ctx := context.Background()
+		// Validate Rego
 
-	r := rego.New(
-		rego.Query("data.example.p"),
-		rego.Module("example_error.rego",
-			`package example
+		ctx := context.Background()
 
-p = true { not q[x] }
-q = {1, 2, 3} { true }`,
-		))
+		r := rego.New(
+			rego.Query("data.example.p"),
+			rego.Module("example_error.rego",
+				inputPolicy,
+			))
 
-	_, err := r.Eval(ctx)
+		_, err := r.Eval(ctx)
 
-	switch err := err.(type) {
-	case ast.Errors:
-		for _, e := range err {
-			fmt.Println("code:", e.Code)
-			fmt.Println("row:", e.Location.Row)
-			fmt.Println("filename:", e.Location.File)
+		switch err := err.(type) {
+		case ast.Errors:
+			for _, e := range err {
+				fmt.Println("code:", e.Code)
+				fmt.Println("row:", e.Location.Row)
+			}
+		default:
+			// Some other error occurred.
 		}
-	default:
-		// Some other error occurred.
-	}
 
+                if err != nil {
+                        fmt.Printf("unable to convert to json %s\n", err)
+                        return err.Error()
+                }
+                return "Error Message"
+        })
+
+        return jsRegoValidateFunc
+}
+
+func main() {
+        fmt.Println("Go Web Assembly")
+	js.Global().Set("validateRegoPolicy", jsRegoValidateWrapper())
+        <-make(chan bool)
 }
